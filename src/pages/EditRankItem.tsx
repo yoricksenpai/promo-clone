786b56a1f4501e2607dd2798d84ef6e9ea2be510
Johan Priso <116643216+yoricksenpai@users.noreply.github.com>
2024-11-16 18:57:43 +0100

    Update EditRankItem.tsx

--- src/pages/EditRankItem.tsx
+++ src/pages/EditRankItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
