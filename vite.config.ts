0b97ea4b619e5701e40ca569a65889101afc00d9
Johan Priso <116643216+yoricksenpai@users.noreply.github.com>
2024-11-16 16:55:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://promo-clone.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
